a57a30f3a40ed7204e7c5d3c9d586ca1339857f9
andrei-pelevin <pelevin72@gmail.com>
2021-06-01 21:34:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function New(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap-item wrap-item-new">
+      <span className="label">New!</span>
+      {props.children}
     </div>
-  );
+  )
+};
+
+function Popular(props) {
+  return (
+    <div className="wrap-item wrap-item-popular">
+      <span className="label">Popular!</span>
+      {props.children}
+    </div>
+  )
+};
+
+function Article(props) {
+  return (
+    <div className="item item-article">
+      <h3><a href="/">{props.title}</a></h3>
+      <p className="views">Прочтений: {props.views}</p>
+    </div>
+  )
+};
+
+function Video(props) {
+  return (
+    <div className="item item-video">
+      <iframe title='newTitle' src={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+      <p className="views">Просмотров: {props.views}</p>
+    </div>
+  )
+};
+
+
+const withUpdatedDisplay = () => {
+  return class extends React.Component {
+    render() {
+      const View = this.props.comp;
+      if (this.props.views < 100) {
+        return <New><View {...this.props} /></New>
+      } else if (this.props.views >= 1000) {
+        return <Popular><View {...this.props} /></Popular>
+      } else {
+        return <View {...this.props} />
+      }
+    }
+  }
 }
 
-export default App;
+const WithUpdatedDisplay = withUpdatedDisplay();
+
+
+function List(props) {
+  return props.list.map((item, i) => {
+    switch (item.type) {
+      case 'video':
+        return (
+          <WithUpdatedDisplay {...item} comp={Video} key={i} />
+        );
+
+      case 'article':
+        return (
+          <WithUpdatedDisplay {...item} comp={Article} key={i} />
+        );
+      default:
+        return '';
+    }
+  });
+};
+
+export default function App() {
+  const [list] = useState([
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 50
+    },
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 12
+    },
+    {
+      type: 'article',
+      title: 'Невероятные события в неизвестном поселке...',
+      views: 175
+    },
+    {
+      type: 'article',
+      title: 'Секретные данные были раскрыты!',
+      views: 1532
+    },
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 4253
+    },
+    {
+      type: 'article',
+      title: 'Кот Бегемот обладает невероятной...',
+      views: 12,
+    },
+  ]);
+
+  return (
+    <List list={list} />
+  );
+}
